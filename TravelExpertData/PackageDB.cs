7e2c7f6d60bfbadfa0814540ece00ec9045f091e
neiladavan <a.neilaldrin@yahoo.com>
2024-07-09 01:26:54 -0600

    Add initial nav menu and data grid view.

---
+++ TravelExpertData/PackageDB.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TravelExpertData
+{
+    public static class PackageDB
+    {
+        public static List<PackageDTO> GetPackages()
+        {
+            using (TravelExpertsContext db = new TravelExpertsContext())
+            { 
+                List<PackageDTO> packages = db.Packages.Select(
+                    p => new PackageDTO
+                    { 
+                        PackageId = p.PackageId,
+                        PkgName = p.PkgName
+                    }).ToList();
+
+                return packages;
+            }
+        }
+    }
+}
---
+++ TravelExpertData/PackageDTO.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TravelExpertData
+{
+    public class PackageDTO
+    {
+        public int PackageId { get; set; }
+
+        public string PkgName { get; set; } = null!;
+
+
+    }
+}
