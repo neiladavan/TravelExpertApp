c78b874180dddc8b2d57c86f79beab29a7f5b7bc
JessicaPLins <jessicaplins@gmail.com>
2024-07-22 19:00:13 -0600

    Set up Supplier section

--- TravelExpertData/SupplierDB.cs
+++ TravelExpertData/SupplierDB.cs
@@ -5,11 +5,40 @@
         public static List<Supplier> GetSuppliers()
         {
             using (TravelExpertsContext db = new TravelExpertsContext())
-            { 
+            {
                 List<Supplier> suppliers = db.Suppliers.ToList();
 
                 return suppliers;
             }
         }
+
+        public static void AddSupplier(Supplier supplier)
+        {
+            using (TravelExpertsContext db = new TravelExpertsContext())
+            {
+                db.Suppliers.Add(supplier);   //adding to the collection of suppliers in the app
+                db.SaveChanges();           //saves changes into the database
+            }
+        }
+
+        public static void ModifySupplier(Supplier supplier)
+        {
+            using (TravelExpertsContext db = new TravelExpertsContext())
+            {
+                db.Suppliers.Update(supplier);    //updatting to the collection of suppliers in the app
+                db.SaveChanges();               //saves changes into the database
+            }
+        }
+
+        /* public static void RemoveSupplier(Supplier supplier)
+         {
+             using (TravelExpertsContext db = new TravelExpertsContext())
+             {
+                 db.Suppliers.Remove(supplier);
+                 db.SaveChanges();
+             }
+         }
+        */
     }
+
 }
--- TravelExpertGUI/frmLandingPage.Designer.cs
+++ TravelExpertGUI/frmLandingPage.Designer.cs
@@ -51,15 +51,17 @@
             panMenu.Controls.Add(btnSuppliers);
             panMenu.Dock = DockStyle.Left;
             panMenu.Location = new Point(0, 0);
+            panMenu.Margin = new Padding(5, 5, 5, 5);
             panMenu.Name = "panMenu";
-            panMenu.Size = new Size(122, 450);
+            panMenu.Size = new Size(198, 720);
             panMenu.TabIndex = 0;
             // 
             // btnPackagesProductsSuppliers
             // 
-            btnPackagesProductsSuppliers.Location = new Point(12, 315);
+            btnPackagesProductsSuppliers.Location = new Point(20, 504);
+            btnPackagesProductsSuppliers.Margin = new Padding(5, 5, 5, 5);
             btnPackagesProductsSuppliers.Name = "btnPackagesProductsSuppliers";
-            btnPackagesProductsSuppliers.Size = new Size(94, 77);
+            btnPackagesProductsSuppliers.Size = new Size(153, 123);
             btnPackagesProductsSuppliers.TabIndex = 4;
             btnPackagesProductsSuppliers.Text = "Package Product Suppliers";
             btnPackagesProductsSuppliers.UseVisualStyleBackColor = true;
@@ -67,9 +69,10 @@
             // 
             // btnProductsSuppliers
             // 
-            btnProductsSuppliers.Location = new Point(12, 224);
+            btnProductsSuppliers.Location = new Point(20, 358);
+            btnProductsSuppliers.Margin = new Padding(5, 5, 5, 5);
             btnProductsSuppliers.Name = "btnProductsSuppliers";
-            btnProductsSuppliers.Size = new Size(94, 50);
+            btnProductsSuppliers.Size = new Size(153, 80);
             btnProductsSuppliers.TabIndex = 3;
             btnProductsSuppliers.Text = "Products Suppliers";
             btnProductsSuppliers.UseVisualStyleBackColor = true;
@@ -77,9 +80,10 @@
             // 
             // btnPackages
             // 
-            btnPackages.Location = new Point(12, 157);
+            btnPackages.Location = new Point(20, 251);
+            btnPackages.Margin = new Padding(5, 5, 5, 5);
             btnPackages.Name = "btnPackages";
-            btnPackages.Size = new Size(94, 30);
+            btnPackages.Size = new Size(153, 48);
             btnPackages.TabIndex = 2;
             btnPackages.Text = "Packages";
             btnPackages.UseVisualStyleBackColor = true;
@@ -87,9 +91,10 @@
             // 
             // btnProducts
             // 
-            btnProducts.Location = new Point(12, 87);
+            btnProducts.Location = new Point(20, 139);
+            btnProducts.Margin = new Padding(5, 5, 5, 5);
             btnProducts.Name = "btnProducts";
-            btnProducts.Size = new Size(94, 30);
+            btnProducts.Size = new Size(153, 48);
             btnProducts.TabIndex = 1;
             btnProducts.Text = "Products";
             btnProducts.UseVisualStyleBackColor = true;
@@ -97,18 +102,21 @@
             // 
             // btnSuppliers
             // 
-            btnSuppliers.Location = new Point(12, 21);
+            btnSuppliers.Location = new Point(20, 34);
+            btnSuppliers.Margin = new Padding(5, 5, 5, 5);
             btnSuppliers.Name = "btnSuppliers";
-            btnSuppliers.Size = new Size(94, 30);
+            btnSuppliers.Size = new Size(153, 48);
             btnSuppliers.TabIndex = 0;
             btnSuppliers.Text = "Suppliers";
             btnSuppliers.UseVisualStyleBackColor = true;
+            btnSuppliers.Click += btnSuppliers_Click;
             // 
             // btnAdd
             // 
-            btnAdd.Location = new Point(128, 336);
+            btnAdd.Location = new Point(208, 538);
+            btnAdd.Margin = new Padding(5, 5, 5, 5);
             btnAdd.Name = "btnAdd";
-            btnAdd.Size = new Size(330, 29);
+            btnAdd.Size = new Size(536, 46);
             btnAdd.TabIndex = 2;
             btnAdd.Text = "Add Button";
             btnAdd.UseVisualStyleBackColor = true;
@@ -116,9 +124,10 @@
             // 
             // btnExit
             // 
-            btnExit.Location = new Point(702, 421);
+            btnExit.Location = new Point(1141, 674);
+            btnExit.Margin = new Padding(5, 5, 5, 5);
             btnExit.Name = "btnExit";
-            btnExit.Size = new Size(94, 29);
+            btnExit.Size = new Size(153, 46);
             btnExit.TabIndex = 3;
             btnExit.Text = "&Exit";
             btnExit.UseVisualStyleBackColor = true;
@@ -127,22 +136,24 @@
             // _mainDataGridView
             // 
             _mainDataGridView.ColumnHeadersHeightSizeMode = DataGridViewColumnHeadersHeightSizeMode.AutoSize;
-            _mainDataGridView.Location = new Point(125, 0);
+            _mainDataGridView.Location = new Point(203, 0);
+            _mainDataGridView.Margin = new Padding(5, 5, 5, 5);
             _mainDataGridView.Name = "_mainDataGridView";
             _mainDataGridView.RowHeadersWidth = 51;
-            _mainDataGridView.Size = new Size(671, 330);
+            _mainDataGridView.Size = new Size(1090, 528);
             _mainDataGridView.TabIndex = 4;
             _mainDataGridView.CellClick += _mainDataGridView_CellClick;
             // 
             // frmLandingPage
             // 
-            AutoScaleDimensions = new SizeF(8F, 20F);
+            AutoScaleDimensions = new SizeF(13F, 32F);
             AutoScaleMode = AutoScaleMode.Font;
-            ClientSize = new Size(800, 450);
+            ClientSize = new Size(1300, 720);
             Controls.Add(_mainDataGridView);
             Controls.Add(btnExit);
             Controls.Add(btnAdd);
             Controls.Add(panMenu);
+            Margin = new Padding(5, 5, 5, 5);
             Name = "frmLandingPage";
             StartPosition = FormStartPosition.CenterScreen;
             Text = "Travel Expert App";
